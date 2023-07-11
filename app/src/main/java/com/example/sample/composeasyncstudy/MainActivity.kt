29bb7180dce1815e3ddb161d4f34c0a313564bda
이후기 <lugan2@naver.com>
2023-07-12 04:35:35 +0900

    Add suspend, StateFlow, Flow practice code to Android Compose

--- app/src/main/java/com/example/sample/composeasyncstudy/MainActivity.kt
+++ app/src/main/java/com/example/sample/composeasyncstudy/MainActivity.kt
@@ -1,109 +1,131 @@
 package com.example.sample.composeasyncstudy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
-import java.util.concurrent.CancellationException
-import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyScreen()
-        }
-    }
-}
-
-@Composable
-fun MainScreen(mainViewModel: MainViewModel = viewModel()) {
-    val name: String by mainViewModel.name.observeAsState("")
-    HelloContent(name = name, onNameChange = { mainViewModel.onNameChange(it) })
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HelloContent(name: String, onNameChange: (String) -> Unit) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "Hello, $name!",
-            modifier = Modifier.padding(bottom = 8.dp),
-            style = MaterialTheme.typography.bodyLarge
-        )
-        OutlinedTextField(
-            value = name,
-            onValueChange = onNameChange,
-            label = { Text("Name") }
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyScreen(mainViewModel: MainViewModel = viewModel()) {
-    val snackBarHostState = remember { SnackbarHostState() }
-
-    val isError by mainViewModel.error.observeAsState(false)
-
-    if(isError) {
-        //에러 발생시 스낵바 호출, CoroutineScope를 통해 코루틴을 실행한다.
-        LaunchedEffect(isError) {
-            try {
-                snackBarHostState.showSnackbar(
-                    message = "Error message",
-                    actionLabel = "Retry message"
-                )
-            } catch (e: CancellationException) {
-                Log.e("composeTest", "canceled!!")
-            }
-        }
-    }
-    
-    Scaffold(snackbarHost = { SnackbarHost(snackBarHostState) }) {
-        Box(modifier = Modifier.padding(it)) {
             MainScreen()
         }
     }
 }
 
-@HiltViewModel
-class MainViewModel @Inject constructor(private val savedInstanceState: SavedStateHandle) : ViewModel() {
-    private val _name = MutableLiveData("")
-    val name: LiveData<String> = _name
 
-    private val _error = MutableLiveData(false)
-    val error: LiveData<Boolean> = _error
 
-    //text 를 입력받아 TextView의 문구를 변경한다.
-    //입력받은 text가 "error"일 경우 error LiveData를 true로 변경한다.
-    fun onNameChange(newName: String) {
-        _name.value = newName
-        _error.value = newName == "error"
+@Composable
+fun MainScreen(mainViewModel: MainViewModel = viewModel()) {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        val text by remember { mainViewModel.textState }
+        var textFlow by remember { mutableStateOf("") }
+        val text2 by remember { mainViewModel.textState2 }
+
+        LaunchedEffect(Unit) {
+            mainViewModel.suspendFunction("hello World!")
+        }
+
+        LaunchedEffect(key1 = mainViewModel) {
+            mainViewModel.textStateFlow.collect {
+                textFlow = it
+            }
+        }
+
+        Text(text = text)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { mainViewModel.suspendFunction("changeEvent") }) {
+            Text(text = "Click me (suspend)")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp).fillMaxWidth().border(border = BorderStroke(width = 16.dp, color = Color.Black)))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = textFlow)
+        Button(onClick = { mainViewModel.emitFlow() }) {
+            Text(text = "Click me (stateFlow collect in view)")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp).fillMaxWidth().border(border = BorderStroke(width = 16.dp, color = Color.Black)))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = text2)
+        Button(onClick = { mainViewModel.collectInViewModel() }) {
+            Text(text = "Click me (flow collect in viewModel)")
+        }
     }
 }
+
+class MainViewModel: ViewModel() {
+    private val _textState = mutableStateOf("loading...")
+    val textState: State<String> = _textState
+
+    private val _textStateFlow = MutableStateFlow("loading...")
+    val textStateFlow = _textStateFlow.asStateFlow()
+
+    private val _textState2 = mutableStateOf("loading...")
+    val textState2: State<String> = _textState2
+
+    init {
+        viewModelScope.launch {
+            suspendFunction("hello World!")
+        }
+    }
+
+    fun suspendFunction(value: String) {
+        viewModelScope.launch {
+            delay(5000L)
+            _textState.value = "$value (after 2 second)"
+        }
+    }
+
+    fun emitFlow() {
+        viewModelScope.launch {
+            listOf("one", "two", "three", "four", "five", "six")
+                .asFlow()
+                .onEach { delay(2000L) }
+                .collect{ _textStateFlow.emit("$it (after 2 second)") }
+        }
+    }
+
+    fun collectInViewModel() {
+        val listFlow = listOf("one", "two", "three", "four", "five", "six").asFlow()
+
+        viewModelScope.launch {
+            listFlow.onEach { delay(2000L) }
+                .collect{ _textState2.value = "$it (after 2 second)" }
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
+
